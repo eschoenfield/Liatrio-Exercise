51c23a96cca2b49cca403ee0dcabf483b432abf2
eschoenfield <eschoenfield@mail.csuchico.edu>
2023-04-13 22:48:56 -0700

    created json object

---
+++ node/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+const info = {
+    "message": "My name is Ely Schoenfield",
+    "timestamp": new Date().getTime()
+}
+
+app.get('/', (req, res) => {
+    res.json(info)
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
